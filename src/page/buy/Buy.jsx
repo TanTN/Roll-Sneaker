a77f4c87491f69b6534ea788e27351ac257f646d
TanTN <ttan6596@gmail.com>
2023-05-27 17:53:08 +0700

    add

--- src/page/buy/Buy.jsx
+++ src/page/buy/Buy.jsx
@@ -96,7 +96,8 @@ const Buy = () => {
                 ...e,
                 district: '',
                 ward: '',
-            },setIsWard([])));
+            }));
+            await setIsWard([])
         };
         fetchApi();
     };
